bc909bf848d328a7bc12b2ddc0e3aacbbea660c0
Dang Nguyen <dtn1712@gmail.com>
2014-09-22 21:15:05 -0700

    add redis to dependency and remove wrong import

--- soulightrd/apps/main/views.py
+++ soulightrd/apps/main/views.py
@@ -1,9 +1,3 @@
-###############################
-#  Author: Dang Nguyen,Duc Vu #
-#  Date: 5/24/2012            #
-#  Last Modified: 10/04/2013  #
-###############################
-
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponseServerError
 from django.template import RequestContext
 from django.shortcuts import render_to_response, get_object_or_404, get_list_or_404
@@ -12,13 +6,6 @@ from django.contrib.auth.models import User, AnonymousUser
 from django.utils import simplejson, timezone
 from django.core import serializers
 
-from frittie.apps.app_helper import get_user_login_object, convert_list_to_json, get_template_path
-from frittie.apps.activity.helper import get_nearby_activity
-from frittie.apps.location.helper import get_nearby_location, get_current_city
-from frittie.apps.main.models import Activity, Location, Photo
-
-from frittie.apps.main.constants import PUBLIC, LOCATION_PRIVATE_PLACE
-from frittie.apps.app_settings import NUM_ITEM_PER_PAGE
 
 import json, logging, datetime
 
