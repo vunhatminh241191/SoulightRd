f47768786e355682019ff668349a4d162e4b7f59
Minh Vu <vnm241191@gmail.com>
2014-11-01 02:18:38 -0700

    Fixing double django-compressor and organization forms

--- soulightrd/apps/organization/forms.py
+++ soulightrd/apps/organization/forms.py
@@ -1,14 +1,10 @@
 from django import forms
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import User
-<<<<<<< HEAD
 
 from soulightrd.apps.main.models import Organization
 
 from cities_light.models import City
-
-from phonenumber_field.modelfields import PhoneNumberField
-=======
 
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
@@ -16,41 +12,11 @@ from crispy_forms.layout import Submit
 from soulightrd.apps.main.models import Organization
 
 from cities_light.models import City
-from phonenumber_field.widgets import PhoneNumberPrefixWidget
->>>>>>> remotes/origin/organization_form
 
 from django_countries.widgets import CountrySelectWidget
 
 class OrganizationSignUpForm(forms.Form):
 	name = forms.CharField(
-<<<<<<< HEAD
-			label = _("Name"), max_length = 50,
-			widget = forms.TextInput(attrs={'placeholder': 'Organization Name'})
-		)
-	description = forms.CharField(
-			label = _("Description "), max_length= 1000,
-			widget = forms.TextInput(attrs={'placeholder': 'Tell us more about your organization'})
-		)
-	website = forms.URLField(
-			label = _("Website"), max_length= 30, required=False,
-			widget = forms.TextInput(attrs={'placeholder': 'Your organization website'})
-		)
-	email = forms.EmailField(
-			label = _("Email"), max_length= 100,
-			widget = forms.TextInput(attrs={'placeholder': 'Your organization email'})
-		)
-	phone = PhoneNumberField()
-	address = forms.CharField(label = _("Address"), max_length = 50,
-			widget = forms.TextInput(attrs={'placeholder': 'Organization Name'})
-		)
-	city = forms.CharField(label = _("City"), max_length = 300,
-			widget=forms.TextInput(attrs={'placeholder': "City"})
-		)
-	country = forms.CharField(label= _("Country"), max_length = 100,
-			widget=CountrySelectWidget
-		)
-
-=======
 			label = _("Name"),
 			widget = forms.TextInput(attrs={'placeholder': 'Organization Name'})
 		)
@@ -89,4 +55,3 @@ class OrganizationSignUpForm(forms.Form):
 		self.helper.form_method = 'post'
 		self.helper.form_action = 'create_organization'
 		self.helper.add_input(Submit('submit', 'Submit'))
->>>>>>> remotes/origin/organization_form
