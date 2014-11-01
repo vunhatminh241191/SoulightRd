b9539fe9ebeca1b9cdfb2abc75c4cf753835e5ee
Minh Vu <vnm241191@gmail.com>
2014-11-01 01:25:38 -0700

    fixing organization form

--- soulightrd/apps/organization/forms.py
+++ soulightrd/apps/organization/forms.py
@@ -1,43 +1,46 @@
 from django import forms
 from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import User
+
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
 
 from soulightrd.apps.main.models import Organization
+
 from cities_light.models import City
-from django.contrib.auth.models import User
+
+from phonenumber_field.modelfields import PhoneNumberField
+
+from django_countries.widgets import CountrySelectWidget
 
 class OrganizationSignUpForm(forms.Form):
 	name = forms.CharField(
-			label = _("Organization Name"), max_length = 50,
-			widget = forms.TextInput(attrs={'placeholder': 'Organization Name'
-				, 'class': "form-control"})
+			label = _("Name"), max_length = 50,
+			widget = forms.TextInput(attrs={'placeholder': 'Organization Name'})
 		)
 	description = forms.CharField(
-			label = _("Description Organization"), max_length= 500,
-			widget = forms.TextInput(attrs={'placeholder': 'Tell us more about your organization'
-				, 'class': "form-control"})
+			label = _("Description "), max_length= 1000,
+			widget = forms.TextInput(attrs={'placeholder': 'Tell us more about your organization'})
 		)
 	website = forms.URLField(
-			label = _("Organization Website"), max_length= 30, required=False,
-			widget = forms.TextInput(attrs={'placeholder': 'Your organization website'
-				, 'class': "form-control"})
+			label = _("Website"), max_length= 30, required=False,
+			widget = forms.TextInput(attrs={'placeholder': 'Your organization website'})
 		)
 	email = forms.EmailField(
-			label = _("Organization Email"), max_length= 100,
-			widget = forms.TextInput(attrs={'placeholder': 'Your organization email'
-				, 'class': "form-control"})
+			label = _("Email"), max_length= 100,
+			widget = forms.TextInput(attrs={'placeholder': 'Your organization email'})
 		)
-	phone = forms.CharField(label = _("Organization Phone"),
-			widget = forms.TextInput(attrs={'placeholder': 'Your organization phone number'
-				, 'class': "form-control"})
+	phone = PhoneNumberField()
+	address = forms.CharField(label = _("Address"), max_length = 50,
+			widget = forms.TextInput(attrs={'placeholder': 'Organization Name'})
 		)
-	address = forms.ModelChoiceField(queryset=City.objects.all(),
-			label = _("Organization Address")
+	city = forms.CharField(label = _("City"), max_length = 300,
+			widget=forms.TextInput(attrs={'placeholder': "City"})
 		)
-	normal_member = forms.ModelMultipleChoiceField(queryset=User.objects.all(),
-			label = _("Invite Member")
+	country = forms.CharField(label= _("Country"), max_length = 100,
+			widget=CountrySelectWidget
 		)
+
 
 	def __init__(self, *args, **kwargs):
 		super(OrganizationSignUpForm, self).__init__(*args, **kwargs)
