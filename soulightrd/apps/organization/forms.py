3a9350c2232b8148e4d261bd2799c4fc5307ab60
Minh Vu <vnm241191@gmail.com>
2014-11-01 01:51:56 -0700

    Fixing phone field in organization form and adding jquery validation to validate name, description, phone

--- soulightrd/apps/organization/forms.py
+++ soulightrd/apps/organization/forms.py
@@ -8,36 +8,38 @@ from crispy_forms.layout import Submit
 from soulightrd.apps.main.models import Organization
 
 from cities_light.models import City
-
-from phonenumber_field.modelfields import PhoneNumberField
+from phonenumber_field.widgets import PhoneNumberPrefixWidget
 
 from django_countries.widgets import CountrySelectWidget
 
 class OrganizationSignUpForm(forms.Form):
 	name = forms.CharField(
-			label = _("Name"), max_length = 50,
+			label = _("Name"),
 			widget = forms.TextInput(attrs={'placeholder': 'Organization Name'})
 		)
 	description = forms.CharField(
-			label = _("Description "), max_length= 1000,
+			label = _("Description "),
 			widget = forms.TextInput(attrs={'placeholder': 'Tell us more about your organization'})
 		)
 	website = forms.URLField(
-			label = _("Website"), max_length= 30, required=False,
+			label = _("Website"), required=False,
 			widget = forms.TextInput(attrs={'placeholder': 'Your organization website'})
 		)
 	email = forms.EmailField(
-			label = _("Email"), max_length= 100,
+			label = _("Email"),
 			widget = forms.TextInput(attrs={'placeholder': 'Your organization email'})
 		)
-	phone = PhoneNumberField()
-	address = forms.CharField(label = _("Address"), max_length = 50,
+	phone = forms.CharField(
+			label= _("Phone"),
+			widget = forms.TextInput(attrs={'placeholder': 'Your organization email'})
+		)
+	address = forms.CharField(label = _("Address"),
 			widget = forms.TextInput(attrs={'placeholder': 'Organization Name'})
 		)
-	city = forms.CharField(label = _("City"), max_length = 300,
+	city = forms.CharField(label = _("City"),
 			widget=forms.TextInput(attrs={'placeholder': "City"})
 		)
-	country = forms.CharField(label= _("Country"), max_length = 100,
+	country = forms.CharField(label= _("Country"),
 			widget=CountrySelectWidget
 		)
 
