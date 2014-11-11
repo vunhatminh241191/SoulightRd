063a3eaa260cafe1ef24c30f2222479ca6ff7fe3
Minh Vu <vnm241191@gmail.com>
2014-11-11 15:13:08 -0800

    Changing organization form format

--- soulightrd/apps/organization/forms.py
+++ soulightrd/apps/organization/forms.py
@@ -7,45 +7,68 @@ from soulightrd.apps.main.models import Organization
 from cities_light.models import City
 
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit
+from crispy_forms.layout import Layout, Fieldset, ButtonHolder, Submit
+from crispy_forms.layout import Div, HTML, Button, Hidden, Field
 
 from soulightrd.apps.main.models import Organization
 
-from cities_light.models import City
-
-from django_countries.widgets import CountrySelectWidget
+from cities_light.models import City, Country
 
 class OrganizationSignUpForm(forms.Form):
 	name = forms.CharField(
 			label = _("Name"),
-			widget = forms.TextInput(attrs={'placeholder': 'Organization Name'})
+			widget = forms.TextInput()
 		)
 	description = forms.CharField(
 			label = _("Description "),
-			widget = forms.TextInput(attrs={'placeholder': 'Tell us more about your organization'})
+			widget = forms.Textarea(attrs={'placeholder': 'Tell us more about your organization',"class": "mce-editor"})
 		)
 	website = forms.URLField(
-			label = _("Website"), required=False,
-			widget = forms.TextInput(attrs={'placeholder': 'Website'})
+			label = _("Website (optional)"), required=False,
+			widget = forms.TextInput()
 		)
 	email = forms.EmailField(
 			label = _("Email"),
-			widget = forms.TextInput(attrs={'placeholder': 'Email'})
+			widget = forms.TextInput()
 		)
 	phone = forms.RegexField(label=_("Phone"),max_length=15,
 			regex=r'^\+?1?\d{9,15}$', 
 			error_message = ("Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed."),
-			widget = forms.TextInput(attrs={'placeholder': 'Phone Number'}))
+			widget = forms.TextInput())
 	address = forms.CharField(label = _("Address"), max_length = 200,
-			widget = forms.TextInput(attrs={'placeholder': 'Organization Name'})
+			widget = forms.TextInput(attrs={'placeholder': 'Street address, district, etc'})
 		)
-	city = forms.ModelChoiceField(label = _("City"), queryset=City.objects.all())
+	city = forms.CharField(label = _("City"), max_length = 10,
+			widget=forms.TextInput()
+		)
+	country = forms.ModelChoiceField(label = _("Country"), queryset=Country.objects.all())
 
 	def __init__(self, *args, **kwargs):
 		super(OrganizationSignUpForm, self).__init__(*args, **kwargs)
 		self.helper = FormHelper()
-		self.helper.form_id = 'signup_form'
-		self.helper.form_class = 'signup'
+		self.helper.form_id = 'create_organization_form'
+		self.helper.form_class = 'form-horizontal'
+		self.helper.label_class = 'col-lg-2'
+		self.helper.field_class = 'col-lg-9'
 		self.helper.form_method = 'post'
 		self.helper.form_action = 'create_organization'
-		self.helper.add_input(Submit('submit', 'Submit'))
+		self.helper.layout = Layout(
+		    'name',
+		    'description',
+		    'email',
+		    'phone',
+		    'address',
+		    'city',
+		    'country',
+		    'website',
+		    Div(
+		    	Div(css_class='col-lg-2'),
+		    	ButtonHolder(
+		    		Button('cancel', 'Cancel',css_class='btn btn-default btn-sm pull-right mini-margin-left'),
+		    		Submit("submit","Submit",css_class="btn btn-primary btn-sm pull-right"),
+		    		css_class = 'col-lg-9'
+		    	),
+		    	css_class = "form-group"
+		    ),
+		    Hidden("city_pk_value","",id="city_pk_value")
+		)
